9e8e9cc5205583406f749661c6087d1244029f17
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-09-14 16:24:39 +0200

    Video component

--- app/renderer_src/App.svelte
+++ app/renderer_src/App.svelte
@@ -1,11 +1,12 @@
 <script>
   import { camera } from "./stores/camera";
+  import Video from "./components/Video.svelte";
+
+  $: stream = $camera && $camera.stream;
 </script>
 
-{#if $camera}
-<div>
-  {$camera.track.label}
-</div>
+{#if stream}
+<Video {stream} height={200} />
 {:else}
 <div>
   Waiting for camera...
---
+++ app/renderer_src/components/Video.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let stream;
+  export let width = null;
+  export let height = 720;
+
+  let video;
+
+  $: if (stream && video) {
+    video.srcObject = stream;
+  };
+</script>
+
+<video bind:this={video} {width} {height} autoplay></video>
