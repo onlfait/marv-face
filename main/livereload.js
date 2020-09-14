e6b163379b423246dbe06e7d972f8e23cc6d46d8
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-09-14 07:03:13 +0200

    refacto...

--- main/livereload.js
+++
@@ -1,7 +0,0 @@
-module.exports = callback => {
-  const path = require("path");
-  const chokidar = require("chokidar");
-  const cwd = path.resolve(__dirname, "..");
-  const watch = ["./main/**/*", "./renderer**/*"];
-  chokidar.watch(watch, { cwd }).on("all", callback);
-};
