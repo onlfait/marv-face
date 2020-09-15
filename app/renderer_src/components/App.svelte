c8a266397c21a867ec55e770652c66b5ce8ace00
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-09-15 07:38:34 +0200

    video feedback & three scene

--- app/renderer_src/components/App.svelte
+++ app/renderer_src/components/App.svelte
@@ -1,5 +1,39 @@
 <script>
-  let width = 600;
+  import { cameraWatcher } from "../modules/camera";
+  import { createScene } from "../modules/three";
+
+  let three;
+  let camera;
+  let videoWrapper;
+  let sceneWrapper;
+
+  const cameraSize = { width: 400, height: 400 };
+  const cameraWatch = cameraWatcher({ video: cameraSize });
+
+  cameraWatch.on("camera", cam => (camera = cam));
+  cameraWatch.on("ended", () => (camera = null));
+  cameraWatch.on("error", console.warn);
+
+  cameraWatch.start();
+
+  $: if (camera && videoWrapper) {
+    three = createScene({ ...cameraSize, video: camera.video });
+    videoWrapper.appendChild(camera.video);
+    sceneWrapper.appendChild(three.renderer.domElement);
+    three.animate();
+  }
 </script>
 
-{width}
+{#if camera}
+<div class="p-2 bg-gray-700">
+  {camera.track.label}
+</div>
+<div class="relative">
+  <div bind:this={videoWrapper} class="absolute"></div>
+  <div bind:this={sceneWrapper} class="absolute"></div>
+</div>
+{:else}
+<div class="p-2 bg-gray-700">
+  Waiting for camera...
+</div>
+{/if}
